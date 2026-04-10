d817fbc6a6125f6b4953a14455d65a4fb4e79bda
drei <159017959+niyeen@users.noreply.github.com>
2026-04-10 14:58:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Welcome to my product page!");
+document.addEventListener("DOMContentLoaded", function() {
+
+  alert("Welcome to my product page!");
+
+  document.getElementById("buyBtn").onclick = function() {
+    alert("Thanks for clicking!");
+  };
+
+  document.getElementById("colorBtn").onclick = function() {
+    document.body.style.backgroundColor = "lightgreen";
+  };
+
+});
